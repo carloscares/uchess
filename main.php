acc1573160fa0bc81f1ee00e4b693ac4b755f1b8
dci <dci@ufro.cl>
2019-10-14 10:52:43 -0300

    actualizando main.php y clase Partida

---
+++ main.php
@@ -0,0 +1,17 @@
+<script>
+<?php
+include_once 'class.Partida.js';
+include_once 'class.Tablero.js';
+include_once 'class.Casilla.js';
+include_once 'class.Pieza.js';
+include_once 'class.Torre.js';
+include_once 'class.Alfil.js';
+include_once 'class.Caballo.js';
+include_once 'class.Peon.js';
+include_once 'class.Dama.js';
+include_once 'class.Rey.js';
+?>
+
+tab = new Partida('juan','alberto');
+tab.tableroActual().despliega();
+</script>
