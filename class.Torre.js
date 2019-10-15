4b0775ffd8a2b1ed924f1d06216d7caf42423677
Sebastian <sapardosuazo@gmail.com>
2019-10-15 10:53:10 -0300

    Arregla funcionalidad de puedeJugar()

--- class.Torre.js
+++ class.Torre.js
@@ -17,38 +17,65 @@ class Torre extends Pieza {
 		let tablero = this.casillaActual.tablero;
 		let id = this.casillaActual.id.split("");
 		// Recorre la columna
-		for (let i = Number(id[1])+1; i <= 8; i++) {
+		let eated = false;
+		for (let i = Number(id[1]) + 1; i <= 8; i++) {
 			let idx = id[0] + i;
 			if (!tablero.casilla[idx].estaVacia()) {
-				break;
+				if (!eated) {
+					eated = true;
+				}
+				else {
+					break;
+				}
 			}
 			posiblesJugadas.push(tablero.casilla[idx].id);
 		}
 
+		eated = false;
 		for (let i = Number(id[1])-1; i >= 1; i--) {
+			let count = 0;
 			let idx = id[0] + i;
 			if (!tablero.casilla[idx].estaVacia()) {
-				break;
+				if (!eated) {
+					eated = true;
+				}
+				else {
+					break;
+				}
 			}
 			posiblesJugadas.push(tablero.casilla[idx].id);
 		}
 
 		// Recorre la fila
+		eated = false
 		let letra = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 		let index = letra.indexOf(id[0]);
 
 		for (let i = Number(index)+1; i < 8; i++) {
+			let count = 0;
 			let idx = letra[i] + id[1];
 			if (!tablero.casilla[idx].estaVacia()) {
-				break;
+				if (!eated) {
+					eated = true;
+				}
+				else {
+					break;
+				}
 			}
 			posiblesJugadas.push(tablero.casilla[idx].id);
 		}
 
+		eated = false;
 		for (let i = Number(index)-1; i >= 0; i--) {
+			let count = 0;
 			let idx = letra[i] + id[1];
 			if (!tablero.casilla[idx].estaVacia()) {
-				break;
+				if (!eated) {
+					eated = true;
+				}
+				else {
+					break;
+				}
 			}
 			posiblesJugadas.push(tablero.casilla[idx].id);
 		}
